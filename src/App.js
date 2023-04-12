a4e243c1668d9f12ac8dafc5901acbf988e2bf33
parthibanchristopher <parthibanchristopher@gmail.com>
2023-04-12 21:35:15 +0800

    created main dashboard and client details pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import MainDashboard from "./Views/MainDashboard";
+import ClientDetails from "./Views/ClientDetails";
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainDashboard />} />
+        <Route path="/client/:id" element={<ClientDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
